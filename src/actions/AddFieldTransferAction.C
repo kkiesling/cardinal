5af1b86f3062f56907c978b99b2bddf3c36d377a
April Novak <novak@berkeley.edu>
2026-02-21 21:32:25 -0600

    init volumetric data transfer

--- src/actions/AddFieldTransferAction.C
+++ src/actions/AddFieldTransferAction.C
@@ -48,8 +48,13 @@ AddFieldTransferAction::act()
       mooseError("The [FieldTransfers] block can only be used with wrapped Nek cases! "
                  "You need to change the [Problem] block to 'NekRSProblem'.");
 
-    _moose_object_pars.set<NekRSProblem *>("_nek_problem") = nek_problem;
-    nek_problem->addObject<FieldTransferBase>(_type, _name, _moose_object_pars, false)[0];
+    if (_type == "NekFieldVariable" || _type == "NekVolumetricSource" ||
+        _type == "NekBoundaryFlux" || _type == "NekMeshDeformation" || _type == "NekVolumetricData")
+    {
+      _moose_object_pars.set<NekRSProblem *>("_nek_problem") = nek_problem;
+      auto transfer =
+          nek_problem->addObject<FieldTransferBase>(_type, _name, _moose_object_pars, false)[0];
+    }
   }
 }
 #endif
